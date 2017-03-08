33aa4e8bc04200fb763a6121cd23288b0503025e
Mims H. Wright <mims@mimswright.com>
2017-03-08 14:40:21 +0100

    streamlined exporting of index file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 // Lodash utils
 import merge from 'lodash/merge'
 
-import {generateAction} from './generateAction'
-import {generateGetter} from './generateGetter'
+import * as generateAction from './generateAction'
+import * as generateGetter from './generateGetter'
 import * as naming from './naming'
 import * as bundle from './bundle'
 import * as reducerGenerators from './generateReducer'
 import * as bundlePresets from './bundlePresets'
 
 let lib = merge(
-  {
-    generateAction,
-    generateGetter
-  },
+  generateAction,
+  generateGetter,
   naming,
   bundle,
   reducerGenerators,
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,8 @@ import {assertIsFunction} from './avaHelpers'
 import * as lib from '../src/index.js'
 
 import * as naming from '../src/naming.js'
+import * as generateAction from '../src/generateAction.js'
+import * as generateGetter from '../src/generateGetter.js'
 import * as bundle from '../src/bundle.js'
 import * as bundlePresets from '../src/bundlePresets.js'
 import * as reducerGenerators from '../src/generateReducer.js'
@@ -10,11 +12,10 @@ import * as reducerGenerators from '../src/generateReducer.js'
 test('index', assert => {
   const check = (funcName) => assertIsFunction(assert, lib[funcName])
 
-  // Check to see that all of these functions exist in the index.
-  check('generateAction')
-  check('generateGetter')
   // Check for all exported objects in each of these sub-modules
   Object.keys(naming).map(check)
+  Object.keys(generateAction).map(check)
+  Object.keys(generateGetter).map(check)
   Object.keys(bundle).map(check)
   Object.keys(bundlePresets).map(check)
   Object.keys(reducerGenerators).map(check)
